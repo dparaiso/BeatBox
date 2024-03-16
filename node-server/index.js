aa8a68fc1af180229f26f14666c263e68ad15e1e
adpop <adpop@debian-11.8.0-beaglebone.myguest.virtualbox.org>
2024-03-15 17:36:21 -0700

    added socket code (not complete, not tested)

--- node-server/index.js
+++ node-server/index.js
@@ -1,62 +1,37 @@
-var PORT_NUMBER = 8088;
+var PORT = 8088;
 
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
-import mime from 'mime';
 
-var server = http.createServer(function(request, response) {
-	var filePath = false;
-	
-	if (request.url == '/') {
-		filePath = 'public/index.html';
+const http = require('http');
+const path = require('path');
+const mime = require('mime');
+const fs   = require('fs');
+
+const server = http.createServer((req, res) => {
+	var filePath = "";
+	if (req.url != '/') {
+		filePath = './public' + req.url;
 	} else {
-		filePath = 'public' + request.url;
+		filePath = './public/index.html';
 	}
-	
-	var absPath = './' + filePath;
-	serveStatic(response, absPath);
+	requestListener(res, filePath);
 });
 
-server.listen(PORT_NUMBER, function() {
-	console.log("Server listeneing on port " + PORT_NUMBER);
+server.listen(PORT, () => {
+	console.log("Server listening on port " + PORT);
 });
 
-function serveStatic(response, absPath) {
-	fs.exists(absPath, function(exists) {
-		if (exists) {
-			fs.readFile(absPath, function(err, data) {
-				if (err) {
-					send404(response);
-				} else {
-					sendFile(response, absPath, data);
-				}
-			});
-		} else {
-			send404(response);
+const requestListener = (res, filePath) => {
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			throw err;
 		}
-	});
-}
+		else {
+			res.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+			res.end(data);
+		}
+	  })
+};
 
-function send404(response) {
-	response.writeHead(404, {'Content-Type': 'text/plain'});
-	response.write('Error 404: resource not found.');
-	response.end();
-}
-
-function sendFile(response, filePath, fileContents) {
-	response.writeHead(
-			200,
-			{"content-type": mime.lookup(path.basename(filePath))}
-		);
-	response.end(fileContents);
-}
-
-
-// /*
-//  * Create the Userver to listen for the websocket
-//  */
-// // var udpServer = require('./lib/udp_server');
-import {listen} from './udpServer.js'
-listen();
+var udpServer = require('./lib/udpServer');
+udpServer.listen(server)
 
---
+++ node-server/public/javascript/beatBoxUi.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var socket = io.connect();
+$(document).ready(() => {
+    $('#btnVolUp').click(() => {
+        var volLvl = Number($('#volLvl').val());
+        if(volLvl < 100-5) {
+            $('#volLvl').val(volLvl+5);
+        }
+        else {
+            $('#volLvl').val(100);
+        }
+        sendCommand("volume " + $('#volLvl').val);
+    });
+    $('#btnVolDown').click(() => {
+        var volLvl = Number($('#volLvl').val());
+        if(volLvl > 5) {
+            $('#volLvl').val(volLvl-5);
+        }
+        else {
+            $('#volLvl').val(0);
+        }
+        sendCommand("volume " + $('#volLvl').val);
+    })
+});
+
+const sendCommand = (msg) => {
+    socket.emit('beatBoxCommand', msg);
+}
